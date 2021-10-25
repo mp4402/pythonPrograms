05a75b07e0dfec83c929184b645876a6febdf932
mp4402 <mariopisquiy@ufm.edu>
2021-10-25 15:31:07 -0600

    adskjfksljf

---
+++ flaskPrograms/pruebaUploads/app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from jinja2 import FileSystemLoader, Environment
+
+UPLOAD_FOLDER = 'C:/Users/mepg1/Documents/GitHub/pythonPrograms/flaskPrograms/pruebaUploads/images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+File_loader = FileSystemLoader("templates")
+env = Environment(loader=File_loader)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    template = env.get_template('index.html')
+    error = None
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return template.render()
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            error = 'No se seleccionó ningua imagen'
+            return template.render()
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Imagen cargada correctamente')
+            return template.render()
+    return template.render(error = error)
+
+if __name__ == '_main_':
+    app.run(debug=True)
