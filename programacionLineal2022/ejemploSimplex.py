078d9107f1a7968178599ea2dee46c0a858620fa
mp4402 <mariopisquiy@ufm.edu>
2022-02-14 13:55:06 -0600

    Pretty print de las tablas

--- programacionLineal2022/ejemploSimplex.py
+++ programacionLineal2022/ejemploSimplex.py
@@ -1,6 +1,8 @@
+from matplotlib.cbook import index_of
 from tabulate import tabulate
 import pandas as pd
 import numpy as np
+import time
 
 def encontrarNegativos(funciones):
     contadorNegativos = False
@@ -29,7 +31,6 @@ def encontrarValoresFinales(funciones,i):
     indiceSolucion = []
     for x in range(i):
         indice = np.where(funciones[:,x] == 1)
-        print(type(indice))
         if not indice[0]:
             indiceSolucion.append(-1)
         else:
@@ -39,9 +40,15 @@ def encontrarValoresFinales(funciones,i):
 opcion = 0
 funcionObjetivo = []
 variables = []
-numeroVariables = int(input("Ingrese el número de variables con las que se trabajará: "))
 condicion = False
+while(condicion == False):
+    try:
+        numeroVariables = int(input("Ingrese el número de variables con las que se trabajará: "))
+        condicion = True
+    except:
+        print("Ingrese una opcion valida")
 
+condicion = False
 while(condicion == False):
     try:
         opcion = int(input("Elija si el problema es de maximización o de minimización\n1. Minimización\n2. Maximización\n"))
@@ -50,16 +57,41 @@ while(condicion == False):
         print("Seleccione una opción valida")
 
 for i in range(numeroVariables):
-    variables.append(str(input("Ingrese el nombre de la variable (texto): ")))
-    funcionObjetivo.append(float(input("Ingrese el valor de la variable " + variables[i] + " de la función objetivo: ")))
+    condicion = False
+    while(condicion == False):
+        try:
+            variables.append(str(input("Ingrese el nombre de la variable (texto): ")))
+            condicion = True
+        except:
+            print("Ingrese una opcion valida")
+    condicion = False
+    while(condicion == False):
+        try:
+            funcionObjetivo.append(float(input("Ingrese el valor de la variable " + variables[i] + " de la función objetivo: ")))
+            condicion = True
+        except:
+            print("Ingrese una opcion valida")
+    
+condicion = False
+while(condicion == False):
+    try:
+        numeroRestrictivas = int(input("Ingrese el número de funciones restrictivas con las que se trabajará: "))
+        condicion = True
+    except:
+        print("Ingrese una opcion valida")
 
-numeroRestrictivas = int(input("Ingrese el número de funciones restrictivas con las que se trabajará: "))
 funcionesRestrictivas = [[0 for x in range(numeroVariables+2)] for y in range(numeroRestrictivas)]
 
 for i in range(numeroRestrictivas):
     for j in range(numeroVariables+2):
         if j == numeroVariables+1:
-            funcionesRestrictivas[i][j] = float(input("Ingrese el valor restrictivo de la función: "))
+            condicion = False
+            while(condicion == False):
+                try:
+                    funcionesRestrictivas[i][j] = float(input("Ingrese el valor restrictivo de la función: "))
+                    condicion = True
+                except:
+                    print("Ingrese una opcion valida")
         elif j == numeroVariables:
             condicion = False
             while(condicion == False):
@@ -69,10 +101,17 @@ for i in range(numeroRestrictivas):
                 except:
                     print("Ingrese una opcion valida")
         else:
-            funcionesRestrictivas[i][j] = float(input("Ingrese el valor de la variable " + variables[j] + " de la función restrictiva " + str(i) + " : "))
+            condicion = False
+            while(condicion == False):
+                try:
+                    funcionesRestrictivas[i][j] = float(input("Ingrese el valor de la variable " + variables[j] + " de la función restrictiva " + str(i+1) + " : "))
+                    condicion = True
+                except:
+                    print("Ingrese una opcion valida")
 
 funciones = [[0 for x in range(numeroVariables+numeroRestrictivas+3)] for y in range(numeroRestrictivas + 1)]
-
+contadorExceso = 1
+contadorHolgura = 1
 posicionVariable = 0
 for i in range(numeroRestrictivas+1):
     if i == 0:
@@ -84,18 +123,25 @@ for i in range(numeroRestrictivas+1):
                 funciones[i][j] = (funcionObjetivo[j]*-1)
         funciones[i][posicionVariable] = 1
         funciones[i][numeroVariables+numeroRestrictivas+1] = 0
+        variables.append("z")
     else:
         posicionVariable+=1
         for j in range(numeroVariables):
             funciones[i][j] = funcionesRestrictivas[i-1][j]
         if funcionesRestrictivas[i-1][numeroVariables] == 1:
             funciones[i][posicionVariable] = -1
+            variables.append("s"+str(contadorExceso))
+            contadorExceso+=1
         else:
             funciones[i][posicionVariable] = 1
+            variables.append("e"+str(contadorHolgura))
+            contadorHolgura+=1
         funciones[i][numeroVariables+numeroRestrictivas+1] = funcionesRestrictivas[i-1][numeroVariables+1]
-
+variables.append("Valor")
+variables.append("Valor")
 funciones = np.matrix(funciones)
-print(funciones)
+df = pd.DataFrame(funciones)
+print(tabulate(df,headers=variables))
 
 while(encontrarNegativos(funciones) != False):
     indice = negativoMenor(funciones)
@@ -113,7 +159,10 @@ while(encontrarNegativos(funciones) != False):
             if funciones[i,indice] != 0:
                 multiplicando = (funciones[i,indice]/funciones[indicePivote,indice])*-1
                 funciones[i]=funciones[i]+(multiplicando*funciones[indicePivote])
-    print(funciones)
+    df = pd.DataFrame(funciones,columns=variables)
+    time.sleep(1)
+    print("\n")
+    print(tabulate(df,headers=variables))
 
 indicesSolucion = encontrarValoresFinales(funciones,numeroVariables)
 soluciones = []
