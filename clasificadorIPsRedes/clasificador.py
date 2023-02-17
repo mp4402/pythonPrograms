e0e4f0f9b30f44b425410b45d9979e1f222dfd31
Mario Enrique Pisquiy Gómez <mariopisquiy@ufm.edu>
2023-02-17 14:51:32 -0600

    Actualizacion Business Intelligence, tarea Redes
    Actualizacion de los notebooks de pandas y pyspark de Business Intelligence y realizacion de la tarea de clasificacion de IPs segun tipo y clase de Introduccion a Redes

---
+++ clasificadorIPsRedes/clasificador.py
@@ -0,0 +1,47 @@
+#Mario Enrique Pisquiy Gómez
+"20200399"
+
+def encontrarPunto(IP):
+    pos = IP.find(".")
+    return pos
+
+def recortarIP(IP, pos):
+    IP = IP[pos+1:len(IP)]
+    return IP
+
+IP = input("Ingrese una IP: ")
+IPprint = IP
+tipo = ""
+clase = ""
+
+posPunto = encontrarPunto(IP)
+octeto1 = IP[0:posPunto]
+IP = recortarIP(IP, posPunto)
+
+posPunto = encontrarPunto(IP)
+octeto2 = IP[0:posPunto]
+IP = recortarIP(IP, posPunto)
+
+if int(octeto1) >= 0 and int(octeto1) <= 126:
+    tipo = "Publica"
+    clase = "A"
+    if int(octeto1) == 10:
+        tipo = "Privada"
+elif int(octeto1) >= 128 and int(octeto1) <= 191:
+    tipo = "Publica"
+    clase = "B"
+    if int(octeto1) == 172 and int(octeto2) >= 16 and int(octeto2) <= 31:
+        tipo = "Privada"
+elif int(octeto1) >= 192 and int(octeto1) <= 223:
+    tipo = "Publica"
+    clase = "C"
+    if int(octeto1) == 192 and int(octeto2) == 168:
+        tipo = "Privada"
+elif int(octeto1) >= 224 and int(octeto1) <= 239:
+    tipo = "Publica"
+    clase = "D"
+elif int(octeto1) >= 240 and (octeto1) <= 254:
+    tipo = "Publica"
+    clase = "E"
+
+print("La IP " + IPprint + " es una IP " + tipo + " de clase " + clase)
