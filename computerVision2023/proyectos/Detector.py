78b8771436dca822091441dfa045848d2c9ef97a
Mario Enrique Pisquiy Gómez <mariopisquiy@ufm.edu>
2023-09-30 19:37:08 -0600

    Modelo y reconocimiento
    Se he creado un modelo de regresión logística (más del 96% de acurracy) y se ha empezado a integrar al programa de detección de placas

--- computerVision2023/proyectos/Detector.py
+++ computerVision2023/proyectos/Detector.py
@@ -6,26 +6,44 @@ import cvlib
 import errno
 import io
 
-def binarizacion(image):
-    imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    binarized = cv.adaptiveThreshold(imgray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,127,0)
-    return binarized
+def imgnorm(img):
+    """Nomalize an image
+    Args:
+        img (numpy array): Source image
+    Returns:
+        normalized (numpy array): Nomalized image
+    """
+    vmin, vmax = img.min(), img.max()
+    normalized_values = []
+    delta = vmax-vmin
+
+    for p in img.ravel():
+        normalized_values.append(255*(p-vmin)/delta)
+
+    normalized  = np.array(normalized_values).astype(np.uint8).reshape(img.shape[0],-1)
+    return normalized
 
 def encontrarContornos(image):
-    mode = cv.RETR_TREE # contour retrieval mode
-    method = [cv.CHAIN_APPROX_NONE, cv.CHAIN_APPROX_SIMPLE] # contour approximation method 
+    mode = cv.RETR_TREE 
+    method = [cv.CHAIN_APPROX_NONE, cv.CHAIN_APPROX_SIMPLE]
     contours, hierarchy = cv.findContours(image, mode, method[0])
     return contours
 
 def encontrarPlaca(image):
-    contours = encontrarContornos(image)
+    imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    normalized = imgnorm(imgray)
+    binarized = cv.adaptiveThreshold(normalized, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV,11,2)
+    contours = encontrarContornos(binarized)
     contorno = []
     caracteristicas = []
     extent = 0
 
     for c in contours:
+        percent =0.052
+        epsilon = percent*cv.arcLength(c,True)
+        approx = cv.approxPolyDP(c,epsilon,True)
         x,y,w,h = cv.boundingRect(c)
-        area = cv.contourArea(c)
+        area = cv.contourArea(approx)
         if (w>h and area > 4000):
             rect_area = w*h
             extent1 = float(area)/rect_area
@@ -38,15 +56,52 @@ def encontrarPlaca(image):
     return placa
 
 def encontrarNumeros(image):
-    contours = encontrarContornos(image)
+    imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    normalized = imgnorm(imgray)
+    binarized = cv.adaptiveThreshold(normalized, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,127,0)
+    cvlib.imgview(binarized)
+    contours = encontrarContornos(binarized)
     contorno = []
     extent = 0
+    direcciones_y = {}
+    tamaño_placa = placa.shape
+    area_min = tamaño_placa[0] * tamaño_placa[1] * 0.008
 
     for c in contours:
+        percent =0.052
+        epsilon = percent*cv.arcLength(c,True)
+        approx = cv.approxPolyDP(c,epsilon,True)
         x,y,w,h = cv.boundingRect(c)
-        area = cv.contourArea(c)
-        if (w<h and area > 300): 
+        area = cv.contourArea(approx)
+        if (w<h and area > area_min):
             contorno.append(c)
+            if (y in direcciones_y):
+                direcciones_y[y].append([x,y,w,h,len(contorno)-1])
+            elif ((y-1) in direcciones_y):
+                direcciones_y[y-1].append([x,y,w,h,len(contorno)-1])
+            elif ((y-2) in direcciones_y):
+                direcciones_y[y-2].append([x,y,w,h,len(contorno)-1])
+            elif ((y-3) in direcciones_y):
+                direcciones_y[y-3].append([x,y,w,h,len(contorno)-1])
+            elif ((y+1) in direcciones_y):
+                direcciones_y[y+1].append([x,y,w,h,len(contorno)-1])
+            elif ((y+2) in direcciones_y):
+                direcciones_y[y+2].append([x,y,w,h,len(contorno)-1])
+            elif ((y+3) in direcciones_y):
+                direcciones_y[y+3].append([x,y,w,h,len(contorno)-1])
+            else:
+                direcciones_y[y] = [[x,y,w,h,len(contorno)-1]]
+
+    keys = []
+    eliminados = 0
+    for key in direcciones_y:
+        if len(direcciones_y[key]) < 2:
+            for element in direcciones_y[key]:
+                contorno.pop(int(element[4]-eliminados))
+                keys.append(key)
+                eliminados = eliminados + 1
+    for key in keys:
+        direcciones_y.pop(key)
 
     return contorno
 
@@ -58,7 +113,6 @@ if __name__ == "__main__":
     color = (0,255,0)
 
     im = cv.imread(args["p"], cv.IMREAD_COLOR)
-    binarized = binarizacion(im)
-    placa = encontrarPlaca(binarized)
-    numeros = encontrarNumeros(binarizacion(placa))
+    placa = encontrarPlaca(im)
+    numeros = encontrarNumeros(placa)
     cvlib.imgview(cv.drawContours(placa.copy(), numeros, -1, color, 1))
